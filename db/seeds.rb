6a065bd262e23c0c8fb33c98236e3ede47253ff3
Sy Rashid <sy.rashid.11@gmail.com>
2021-03-08 01:47:50 -0500

    ARCH: Set up base models for chat application

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Chatroom.destroy_all
+User.destroy_all
+
+Chatroom.create(name: "general")
+User.create(email: "sy@mangotree.dev", nickname: "syrashid", password: "password")
+User.create(email: "nico@mangotree.dev", nickname: "nicoproto", password: "password")
